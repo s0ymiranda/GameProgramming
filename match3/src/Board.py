44ce3aa645a2b1be76fe6175de373d0567d1e7ec
Gerardo Sayago <gasayagom@gmail.com>
2023-03-05 18:57:01 -0400

    Match3 Added

---
+++ match3/src/Board.py
@@ -0,0 +1,336 @@
+"""
+ISPPJ1 2023
+Study Case: Match-3
+
+Author: Alejandro Mujica
+alejandro.j.mujic4@gmail.com
+
+This file contains the class Board.
+"""
+from typing import List, Optional, Tuple, Any, Dict, Set
+
+import pygame
+
+import random
+
+import settings
+from src.Tile import Tile
+
+
+class Board:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+        self.possible_matches: List[List[Tile]] = []
+        self.matches: List[List[Tile]] = []
+        self.tiles: List[List[Tile]] = []
+        self.__initialize_tiles()
+        while not self.ExistMatch():
+            self.__initialize_tiles()   
+
+    def render(self, surface: pygame.Surface) -> None:
+        for row in self.tiles:
+            for tile in row:
+                tile.render(surface, self.x, self.y)
+
+    def __is_match_generated(self, i: int, j: int, color: int) -> bool:
+        if (
+            i >= 2
+            and self.tiles[i - 1][j].color == color
+            and self.tiles[i - 2][j].color == color
+        ):
+            return True
+
+        return (
+            j >= 2
+            and self.tiles[i][j - 1].color == color
+            and self.tiles[i][j - 2].color == color
+        )
+
+    def __initialize_tiles(self) -> None:
+        self.tiles = [
+            [None for _ in range(settings.BOARD_WIDTH)]
+            for _ in range(settings.BOARD_HEIGHT)
+        ]
+        for i in range(settings.BOARD_HEIGHT):
+            for j in range(settings.BOARD_WIDTH):
+                color = random.randint(0, settings.NUM_COLORS - 1)
+                while self.__is_match_generated(i, j, color):
+                    color = random.randint(0, settings.NUM_COLORS - 1)
+
+                self.tiles[i][j] = Tile(
+                    i, j, color, random.randint(0, settings.NUM_VARIETIES - 1)
+                )
+
+    def RecreateBoard(self):
+        self.__initialize_tiles()
+
+    def __calculate_match_rec(self, tile: Tile) -> Set[Tile]:
+        if tile in self.in_stack:
+            return []
+
+        self.in_stack.add(tile)
+
+        color_to_match = tile.color
+
+        ## Check horizontal match
+        h_match: List[Tile] = []
+
+        # Check left
+        if tile.j > 0:
+            left = max(0, tile.j - 2)
+            for j in range(tile.j - 1, left - 1, -1):
+                if self.tiles[tile.i][j].color != color_to_match:
+                    break
+                h_match.append(self.tiles[tile.i][j])
+
+        # Check right
+        if tile.j < settings.BOARD_WIDTH - 1:
+            right = min(settings.BOARD_WIDTH - 1, tile.j + 2)
+            for j in range(tile.j + 1, right + 1):
+                if self.tiles[tile.i][j].color != color_to_match:
+                    break
+                h_match.append(self.tiles[tile.i][j])
+
+        ## Check vertical match
+        v_match: List[Tile] = []
+
+        # Check top
+        if tile.i > 0:
+            top = max(0, tile.i - 2)
+            for i in range(tile.i - 1, top - 1, -1):
+                if self.tiles[i][tile.j].color != color_to_match:
+                    break
+                v_match.append(self.tiles[i][tile.j])
+
+        # Check bottom
+        if tile.i < settings.BOARD_HEIGHT - 1:
+            bottom = min(settings.BOARD_HEIGHT - 1, tile.i + 2)
+            for i in range(tile.i + 1, bottom + 1):
+                if self.tiles[i][tile.j].color != color_to_match:
+                    break
+                v_match.append(self.tiles[i][tile.j])
+
+        match: List[Tile] = []
+
+        if len(h_match) >= 2:
+            for t in h_match:
+                if t not in self.in_match:
+                    self.in_match.add(t)
+                    match.append(t)
+
+        if len(v_match) >= 2:
+            for t in v_match:
+                if t not in self.in_match:
+                    self.in_match.add(t)
+                    match.append(t)
+
+        if len(match) > 0:
+            if tile not in self.in_match:
+                self.in_match.add(tile)
+                match.append(tile)
+
+        for t in match:
+            match += self.__calculate_match_rec(t)
+
+        self.in_stack.remove(tile)
+        return match
+
+    def calculate_matches_for(
+        self, new_tiles: List[Tile]
+    ) -> Optional[List[List[Tile]]]:
+        self.in_match: Set[Tile] = set()
+        self.in_stack: Set[Tile] = set()
+
+        for tile in new_tiles:
+            if tile in self.in_match:
+                continue
+            match = self.__calculate_match_rec(tile)
+            if len(match) > 0:
+                self.matches.append(match)
+
+        delattr(self, "in_match")
+        delattr(self, "in_stack")
+
+        return self.matches if len(self.matches) > 0 else None
+
+    def remove_matches(self) -> None:
+        for match in self.matches:
+            for tile in match:
+                self.tiles[tile.i][tile.j] = None
+
+        self.matches = []
+
+    def cross_power_up(self,tile_i: int,tile_j : int):
+        for j in range(8):
+            self.tiles[tile_i][j] = None
+        for i in range(8):
+            self.tiles[i][tile_j] = None
+
+    def color_power_up(self,color : int) -> int:
+        quantity = 0
+        for i in range(8):
+            for j in range(8):
+                if self.tiles[i][j].color == color:
+                    self.tiles[i][j] = None
+                    quantity += 1
+        return quantity
+
+    def get_falling_tiles(self) -> Tuple[Any, Dict[str, Any]]:
+        # List of tweens to create
+        tweens: Tuple[Tile, Dict[str, Any]] = []
+
+        # for each column, go up tile by tile until we hit a space
+        for j in range(settings.BOARD_WIDTH):
+            space = False
+            space_i = -1
+            i = settings.BOARD_HEIGHT - 1
+
+            while i >= 0:
+                tile = self.tiles[i][j]
+
+                # if our previous tile was a space
+                if space:
+                    # if the current tile is not a space
+                    if tile is not None:
+                        self.tiles[space_i][j] = tile
+                        tile.i = space_i
+
+                        # set its prior position to None
+                        self.tiles[i][j] = None
+
+                        tweens.append((tile, {"y": tile.i * settings.TILE_SIZE}))
+                        space = False
+                        i = space_i
+                        space_i = -1
+                elif tile is None:
+                    space = True
+
+                    if space_i == -1:
+                        space_i = i
+
+                i -= 1
+
+        # create a replacement tiles at the top of the screen
+        for j in range(settings.BOARD_WIDTH):
+            for i in range(settings.BOARD_HEIGHT):
+                tile = self.tiles[i][j]
+
+                if tile is None:
+                    tile = Tile(
+                        i,
+                        j,
+                        random.randint(0, settings.NUM_COLORS - 1),
+                        random.randint(0, settings.NUM_VARIETIES - 1),
+                    )
+                    tile.y -= settings.TILE_SIZE
+                    self.tiles[i][j] = tile
+                    tweens.append((tile, {"y": tile.i * settings.TILE_SIZE}))
+
+        return tweens
+
+    def WillThereBeAMatch(self, tile, i,j):
+
+        same_color = [False,False]
+        h_match = []
+        v_match = []
+        h_min_max = [max(0,j-2),min(7, j + 2)]
+        v_min_max = [max(0,i-2),min(7, i + 2)]
+
+        self.swap_tiles(i,j,tile.i,tile.j)
+
+        while h_min_max[0] <= h_min_max[1]:
+            h_match.append(self.tiles[i][h_min_max[0]])
+            h_min_max[0] += 1
+
+        h = 0
+        in_row = 0
+        color_index = []
+        while h in range(len(h_match)):
+            if h_match[h].color == tile.color:
+                color_index.append(h)
+                in_row += 1
+            else:
+                in_row = 0
+                color_index.clear()
+            h += 1
+            if in_row >= 3:
+                same_color[0] = True
+                break
+
+        g = 0
+        if len(color_index) >= 3:
+            while g < len(color_index):
+                h_match[color_index[g]].can_match = True
+                g += 1
+
+
+        #now vertically
+
+        while v_min_max[0] <= v_min_max[-1]:
+            v_match.append(self.tiles[v_min_max[0]][j])
+            v_min_max[0] += 1
+
+        color_index.clear()
+
+        v = 0
+        in_row = 0
+        while v in range(len(v_match)):
+            if v_match[v].color == tile.color:
+                in_row += 1
+                color_index.append(v)
+            else:
+                in_row = 0
+                color_index.clear()
+            v += 1
+            if in_row >= 3:
+                same_color[1] = True
+                break
+
+        g = 0
+        if len(color_index) >= 3:
+            while g < len(color_index):
+                v_match[color_index[g]].can_match = True
+                g += 1
+
+        self.swap_tiles(i,j,tile.i,tile.j)
+
+        return any(same_color)
+
+    def CanMove(self, tile, i,j)-> bool:
+        #tile is original tile, and i,j are coord to move that tile
+        if j < 0 or j > 8 or i < 0 or i > 8:
+            return False
+
+        return self.WillThereBeAMatch(tile,i,j) or self.WillThereBeAMatch(self.tiles[i][j],tile.i,tile.j)
+
+    def ExistMatch(self):
+        for i in range(8):
+            for j in range(8):
+                tile = self.tiles[i][j]
+                if tile.variety >= 7:
+                    return True
+        for i in range(7):
+            for j in range(7):
+                tile = self.tiles[i][j]
+                if self.CanMove(tile, i - 1, j) or self.CanMove(tile, i + 1, j) or self.CanMove(tile, i, j - 1) or self.CanMove(tile, i, j + 1):
+                    return True
+        return False
+
+    def swap_tiles(self,i1,j1,i2,j2):
+        (
+            self.tiles[i1][j1],
+            self.tiles[i2][j2],
+        ) = (
+            self.tiles[i2][j2],
+            self.tiles[i1][j1],
+        )
+
+    def MarkMatches(self):
+        for i in range(8):
+            for j in range(8):
+                self.tiles[i][j].can_match = False
+        for i in range(7):
+            for j in range(7):
+                tile = self.tiles[i][j]
+                test = self.CanMove(tile, i - 1, j) or self.CanMove(tile, i + 1, j) or self.CanMove(tile, i, j - 1) or self.CanMove(tile, i, j + 1)
---
+++ match3/src/states/PlayState.py
@@ -0,0 +1,406 @@
+"""
+ISPPJ1 2023
+Study Case: Match-3
+
+Author: Alejandro Mujica
+alejandro.j.mujic4@gmail.com
+
+This file contains the class PlayState.
+"""
+from typing import Dict, Any, List
+
+import pygame
+
+from gale.input_handler import InputHandler, InputData
+from gale.state_machine import BaseState
+from gale.text import render_text
+from gale.timer import Timer
+from src.Tile import Tile
+
+import settings
+
+
+class PlayState(BaseState):
+    def enter(self, **enter_params: Dict[str, Any]) -> None:
+        self.level = enter_params["level"]
+        self.board = enter_params["board"]
+        self.score = enter_params["score"]
+
+        #start testing
+        self.on_motion = False
+        self.selected_tile_i = 0
+        self.selected_tile_j = 0
+        self.already_moving = "none"
+        self.tile1 = None
+        self.tile2 = None
+        self.original_x_and_y_tile1 = None
+        self.original_x_and_y_tile2 = None
+
+        self.time_waiting = 0
+        #end testing
+
+        # Position in the grid which we are highlighting
+        self.board_highlight_i1 = -1
+        self.board_highlight_j1 = -1
+        self.board_highlight_i2 = -1
+        self.board_highlight_j2 = -1
+
+        self.highlighted_tile = False
+
+        self.active = True
+
+        self.timer = settings.LEVEL_TIME
+
+        self.goal_score = self.level * 1.25 * 1000
+
+        # A surface that supports alpha to highlight a selected tile
+        self.tile_alpha_surface = pygame.Surface(
+            (settings.TILE_SIZE, settings.TILE_SIZE), pygame.SRCALPHA
+        )
+        pygame.draw.rect(
+            self.tile_alpha_surface,
+            (255, 255, 255, 96),
+            pygame.Rect(0, 0, settings.TILE_SIZE, settings.TILE_SIZE),
+            border_radius=7,
+        )
+
+        # A surface that supports alpha to draw behind the text.
+        self.text_alpha_surface = pygame.Surface((212, 136), pygame.SRCALPHA)
+        pygame.draw.rect(
+            self.text_alpha_surface, (56, 56, 56, 234), pygame.Rect(0, 0, 212, 136)
+        )
+
+        def decrement_timer():
+            self.timer -= 1
+            self.time_waiting += 1
+            # Play warning sound on timer if we get low
+            if self.timer <= 5:
+                settings.SOUNDS["clock"].play()
+
+        Timer.every(1, decrement_timer)
+
+        InputHandler.register_listener(self)
+
+    def exit(self) -> None:
+        InputHandler.unregister_listener(self)
+
+    def update(self, _: float) -> None:
+
+        if self.timer <= 0:
+            Timer.clear()
+            settings.SOUNDS["game-over"].play()
+            self.state_machine.change("game-over", score=self.score)
+
+        if self.score >= self.goal_score:
+            Timer.clear()
+            settings.SOUNDS["next-level"].play()
+            self.state_machine.change("begin", level=self.level + 1, score=self.score)
+
+        while not self.board.ExistMatch():
+            self.board.RecreateBoard()
+            self.board.MarkMatches()
+
+    def render(self, surface: pygame.Surface) -> None:
+        self.board.render(surface)
+
+        if not self.on_motion and self.time_waiting >= 1:
+            for g in range(8):
+                for a in range(8):
+                    tile = self.board.tiles[g][a]
+                    if tile.can_match:
+                        x = tile.j * settings.TILE_SIZE + self.board.x
+                        y = tile.i * settings.TILE_SIZE + self.board.y
+                        surface.blit(self.tile_alpha_surface, (x, y))                    
+
+        surface.blit(self.text_alpha_surface, (16, 16))
+        render_text(
+            surface,
+            f"Level: {self.level}",
+            settings.FONTS["medium"],
+            30,
+            24,
+            (99, 155, 255),
+            shadowed=True,
+        )
+        render_text(
+            surface,
+            f"Score: {self.score}",
+            settings.FONTS["medium"],
+            30,
+            52,
+            (99, 155, 255),
+            shadowed=True,
+        )
+        render_text(
+            surface,
+            f"Goal: {self.goal_score}",
+            settings.FONTS["medium"],
+            30,
+            80,
+            (99, 155, 255),
+            shadowed=True,
+        )
+        render_text(
+            surface,
+            f"Timer: {self.timer}",
+            settings.FONTS["medium"],
+            30,
+            108,
+            (99, 155, 255),
+            shadowed=True,
+        )
+
+    def on_input(self, input_id: str, input_data: InputData) -> None:
+        if not self.active:
+            return
+
+        if input_id == "motion_left" and self.on_motion:
+            if self.already_moving == "up" or self.already_moving == "down":
+                return
+            elif self.already_moving == "none":
+                if self.selected_tile_j-1 >= 0:
+                    self.already_moving = "left"
+                    self.tile2 = self.board.tiles[self.selected_tile_i][self.selected_tile_j-1]
+                    self.original_x_and_y_tile2 = self.tile2.x,self.tile2.y
+                else:
+                    return
+            elif self.already_moving == "right":
+                self.tile2 = self.board.tiles[self.selected_tile_i][self.selected_tile_j+1] 
+            
+            # Swap tiles
+            x, y = input_data.position
+            x = x * settings.VIRTUAL_WIDTH // settings.WINDOW_WIDTH - self.board.x - 16
+            dif = self.tile1.x - x
+            self.tile1.x = self.tile1.x - dif
+            self.tile2.x = self.tile2.x + dif
+
+        if input_id == "motion_right" and self.on_motion:
+            if self.already_moving == "up" or self.already_moving == "down":
+                return
+            elif self.already_moving == "none":
+                if self.selected_tile_j+1 < 8:
+                    self.already_moving = "right"
+                    self.tile2 = self.board.tiles[self.selected_tile_i][self.selected_tile_j+1]
+                    self.original_x_and_y_tile2 = self.tile2.x,self.tile2.y
+                else: 
+                    return
+            elif self.already_moving == "left":
+                self.tile2 = self.board.tiles[self.selected_tile_i][self.selected_tile_j-1]
+            
+            # Swap tiles
+            x, y = input_data.position
+            x = x * settings.VIRTUAL_WIDTH // settings.WINDOW_WIDTH - self.board.x - 16
+            dif = self.tile1.x - x
+            self.tile1.x = self.tile1.x - dif
+            self.tile2.x = self.tile2.x + dif
+   
+        if input_id == "motion_down" and self.on_motion:
+            if self.already_moving == "left" or self.already_moving == "right":
+                return
+            elif self.already_moving == "none":
+                if self.selected_tile_i + 1 < 8:
+                    self.already_moving = "down"
+                    self.tile2 = self.board.tiles[self.selected_tile_i+1][self.selected_tile_j]
+                    self.original_x_and_y_tile2 = self.tile2.x,self.tile2.y
+                else:
+                    return
+            elif self.already_moving == "up":
+                self.tile2 = self.board.tiles[self.selected_tile_i-1][self.selected_tile_j]
+            
+            # Swap tiles
+            x, y = input_data.position
+            y = y * settings.VIRTUAL_HEIGHT // settings.WINDOW_HEIGHT - self.board.y - 16
+            dif = self.tile1.y - y
+            if (self.tile1.x,self.tile1.y) <= (self.original_x_and_y_tile2):
+                self.tile1.y = self.tile1.y - dif
+                self.tile2.y = self.tile2.y + dif
+            
+
+        if input_id == "motion_up" and self.on_motion:
+            if self.already_moving == "left" or self.already_moving == "right":
+                return
+            elif self.already_moving == "none":
+                if self.selected_tile_i - 1 >= 0:
+                    self.already_moving = "up"
+                    self.tile2 = self.board.tiles[self.selected_tile_i-1][self.selected_tile_j]
+                    self.original_x_and_y_tile2 = self.tile2.x,self.tile2.y
+                else:
+                    return
+            elif self.already_moving == "down":
+                self.tile2 = self.board.tiles[self.selected_tile_i+1][self.selected_tile_j]
+
+            # Swap tiles
+            x, y = input_data.position
+            y = y * settings.VIRTUAL_HEIGHT // settings.WINDOW_HEIGHT - self.board.y - 16
+            dif = self.tile1.y - y
+            if (self.tile1.x,self.tile1.y) >= (self.original_x_and_y_tile2):
+                self.tile1.y = self.tile1.y - dif
+                self.tile2.y = self.tile2.y + dif
+
+        if input_id == "click" and input_data.released:
+
+            if self.already_moving == "none":
+                if self.tile1.variety == 7:
+                    self.cross_power_up()
+                elif self.tile1.variety == 8:
+                    self.color_power_up()
+                self.on_motion = False
+                return
+
+            def exchange(x1,y1,x2,y2):
+                Timer.tween(
+                    0.25,
+                    [
+                        (self.tile1, {"x": x1, "y": y1}),
+                        (self.tile2, {"x": x2, "y": y2}),
+                    ],
+                )
+
+                (
+                    self.board.tiles[self.tile1.i][self.tile1.j],
+                    self.board.tiles[self.tile2.i][self.tile2.j],
+                ) = (
+                    self.board.tiles[self.tile2.i][self.tile2.j],
+                    self.board.tiles[self.tile1.i][self.tile1.j],
+                )
+                
+                self.tile1.i, self.tile1.j, self.tile2.i, self.tile2.j = (
+                    self.tile2.i,
+                    self.tile2.j,
+                    self.tile1.i,
+                    self.tile1.j,
+                )
+                self.__calculate_matches([self.tile1, self.tile2])
+                self.active = True
+
+            def not_exchange(x1,y1,x2,y2):
+                Timer.tween(
+                    0.25,
+                    [
+                        (self.tile1, {"x": x1, "y": y1}),
+                        (self.tile2, {"x": x2, "y": y2}),
+                    ],
+                )
+                self.active = True
+
+            self.active = False
+            self.on_motion = False
+
+            x1,y1 = self.original_x_and_y_tile1
+            x2,y2 = self.original_x_and_y_tile2
+
+            flag = self.board.CanMove(self.tile1,self.tile2.i,self.tile2.j)
+
+            if self.already_moving == "left":
+                if self.tile1.x < self.tile2.x and flag:
+                    exchange(x2,y2,x1,y1)
+                else:
+                    not_exchange(x1,y1,x2,y2)
+            elif self.already_moving == "right":
+                if self.tile1.x > self.tile2.x and flag:
+                    exchange(x2,y2,x1,y1)
+                else:
+                    not_exchange(x1,y1,x2,y2)
+            elif self.already_moving == "down":
+                if self.tile1.y > self.tile2.y and flag:
+                    exchange(x2,y2,x1,y1)
+                else:
+                    not_exchange(x1,y1,x2,y2)
+            elif self.already_moving == "up":
+                if self.tile1.y < self.tile2.y and flag:
+                    exchange(x2,y2,x1,y1)
+                else:
+                    not_exchange(x1,y1,x2,y2)
+            self.already_moving = "none"
+
+            self.board.MarkMatches()
+            
+        if input_id == "click" and input_data.pressed:
+            self.on_motion = True
+            pos_x, pos_y = input_data.position
+            pos_x = pos_x * settings.VIRTUAL_WIDTH // settings.WINDOW_WIDTH
+            pos_y = pos_y * settings.VIRTUAL_HEIGHT // settings.WINDOW_HEIGHT
+            i = (pos_y - self.board.y) // settings.TILE_SIZE
+            j = (pos_x - self.board.x) // settings.TILE_SIZE    
+            self.selected_tile_i = i 
+            self.selected_tile_j = j    
+            self.tile1 = self.board.tiles[self.selected_tile_i][self.selected_tile_j]
+            self.original_x_and_y_tile1 = self.tile1.x, self.tile1.y
+            
+            self.time_waiting = 0
+
+    def cross_power_up(self):
+        settings.SOUNDS["match"].stop()
+        settings.SOUNDS["match"].play()   
+        self.board.cross_power_up(self.tile1.i,self.tile1.j)
+        self.score += 15*25             
+        falling_tiles = self.board.get_falling_tiles()
+        Timer.tween(
+            0.5,
+            falling_tiles,
+            on_finish=lambda: self.__calculate_matches(
+                [item[0] for item in falling_tiles]
+            ),
+        )
+
+    def color_power_up(self):
+        settings.SOUNDS["match"].stop()
+        settings.SOUNDS["match"].play()   
+        quantity = self.board.color_power_up(self.tile1.color)
+        self.score += quantity*50             
+        falling_tiles = self.board.get_falling_tiles()
+        Timer.tween(
+            0.5,
+            falling_tiles,
+            on_finish=lambda: self.__calculate_matches(
+                [item[0] for item in falling_tiles]
+            ),
+        )
+
+
+    def __calculate_matches(self, tiles: List) -> None:
+        matches = self.board.calculate_matches_for(tiles)
+
+        if matches is None:
+            self.active = True
+            return
+
+        settings.SOUNDS["match"].stop()
+        settings.SOUNDS["match"].play()
+
+        num = 0
+
+        for match in matches:
+            self.score += len(match) * 50
+            if num < len(match):
+                num = len(match)
+
+        for match in matches:
+            for t in match:
+                if t.variety == 7:
+                    self.cross_power_up()
+                elif t.variety == 8:
+                    self.color_power_up()
+
+        self.board.remove_matches()
+        
+        if num == 4:
+            self.board.tiles[self.tile1.i][self.tile1.j] = Tile(
+                self.tile1.i, self.tile1.j, self.tile1.color,7
+            )
+        elif num > 4:
+            self.board.tiles[self.tile1.i][self.tile1.j] = Tile(
+                self.tile1.i, self.tile1.j, self.tile1.color,8
+            )
+        
+        falling_tiles = self.board.get_falling_tiles()
+
+        Timer.tween(
+            0.25,
+            falling_tiles,
+            on_finish=lambda: self.__calculate_matches(
+                [item[0] for item in falling_tiles]
+            ),
+        )
+
+        self.board.MarkMatches()

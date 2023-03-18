065a592e8acaa2d3991bbd012d9b054515902ce0
Andrea Miranda <andreamirandacas@gmail.com>
2023-03-18 19:53:24 -0400

    Level 2

--- SuperMartian/src/states/game_states/BeginGameState.py
+++ SuperMartian/src/states/game_states/BeginGameState.py
@@ -29,27 +29,15 @@ class BeginGameState(BaseState):
         Timer.tween(
             1,
             [(self, {"transition_alpha": 0})],
-            # once that is finished, start a transition of our text label to
-            # center of the screen over 0.25 seconds
-            on_finish=lambda: Timer.tween(
-                0.25,
-                [(self, {"level_transition": settings.VIRTUAL_HEIGHT // 2 - 30})],
-                # after that, pause for 1.5 second with Timer.after
-                on_finish=lambda: Timer.after(
-                    1.5,
-                    # Then, animate the label going down past the bottom edge
-                    lambda: Timer.tween(
-                        0.25,
-                        [(self, {"level_transition": settings.VIRTUAL_HEIGHT + 30})],
-                        # We are ready to play
-                        on_finish=lambda: self.state_machine.change(
-                            "play", level=2
-                        ),
-                    ),
-                ),
+            on_finish=lambda:Timer.tween(
+                1,
+                [(self, {"transition_alpha": 255})],
+                on_finish=lambda: self.state_machine.change(
+                    "play", level=2
+                )
             ),
-        )
-
+        ),
+            
     def render(self, surface: pygame.Surface) -> None:
 
 
